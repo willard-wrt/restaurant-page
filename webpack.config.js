a6b4217c17f5438568c21c3614cfeedfe9236aea
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2023-02-28 08:29:24 +0700

    Remove dist folder and add styles to the homepage

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,17 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext]',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
